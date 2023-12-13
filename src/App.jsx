8fbe0d55f5ab91c6355b17c02f0a6672eb62c67f
karl-jerico <jerico.jeks@gmail.com>
2023-12-13 17:43:16 +0800

    Add game board feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
+
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+      currentPlayer = 'O'
+    }
+
+    return currentPlayer;
+}
 
 function App() {
-  
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer}, 
+        ...prevTurns,
+      ];
+      
+      return updatedTurns;
+    });
+  };
 
   return <main>
     <div id="game-container">
-      <ol id="players">
-        <Player initialName="Player 1" symbol="X"/>
-        <Player initialName="Player 2" symbol="O"/>
+      <ol id="players" className="highlight-player">
+        <Player isActive={activePlayer === 'X'} initialName="Player 1" symbol="X"/>
+        <Player isActive={activePlayer === 'O'} initialName="Player 2" symbol="O"/>
       </ol>
-      MIKCOOOOOOOOOOOOOO
+      <GameBoard 
+        onSelectSquare={handleSelectSquare} 
+        turns={gameTurns}
+      />
     </div>
+    <Log turns={gameTurns}/>
   </main>;
 }
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 
-const Player = ({initialName, symbol}) => {
+const Player = ({initialName, symbol, isActive}) => {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -14,7 +14,7 @@ const Player = ({initialName, symbol}) => {
     }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
         <span className="player">
             {!isEditing
                 ? <span className="player-name">{playerName}</span>
